60e8aa267f8f1a119ac72a4255e17534ebf6b05d
Lucy <arfiedler1114@gmail.com>
2026-01-29 07:55:45 -0600

    add mmp3 lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 /**
- * @file streams-audiokit-audiokit.ino
- * @author Phil Schatzmann
- * @brief see https://github.com/pschatzmann/arduino-audio-tools/blob/main/examples/examples-audiokit/streams-audiokit-audiokit/README.md
- * 
+ * @file player-sd-audiokit.ino
+ * @brief see https://github.com/pschatzmann/arduino-audio-tools/blob/main/examples/examples-audiokit/player-sd-audiokit/README.md
+ * Make sure that the pins are set to off, on, on, off, off
  * @author Phil Schatzmann
  * @copyright GPLv3
  */
@@ -10,22 +9,58 @@
 
 #include "AudioTools.h"
 #include "AudioTools/AudioLibs/AudioBoardStream.h"
+#include "AudioTools/Disk/AudioSourceSD.h" // or AudioSourceIdxSD.h
+#include "AudioTools/AudioCodecs/CodecMP3Helix.h"
 
-AudioBoardStream kit(AudioKitEs8388V1); // Access I2S as stream
-StreamCopy copier(kit, kit); // copy kit to kit
+const char *startFilePath="/";
+const char* ext="mp3";
+AudioSourceSD source(startFilePath, ext, PIN_AUDIO_KIT_SD_CARD_CS);
+AudioBoardStream kit(AudioKitEs8388V1);
+MP3DecoderHelix decoder;  // or change to MP3DecoderMAD
+AudioPlayer player(source, kit, decoder);
 
-// Arduino Setup
-void setup(void) {
-    Serial.begin(115200);
-    AudioToolsLogger.begin(Serial, AudioToolsLogLevel::Warning);
-    
-    auto cfg = kit.defaultConfig(RXTX_MODE);
-    cfg.sd_active = false;
-    cfg.input_device = ADC_INPUT_LINE2;
-    kit.begin(cfg);
+void next(bool, int, void*) {
+   player.next();
 }
 
-// Arduino loop - copy data
+void previous(bool, int, void*) {
+   player.previous();
+}
+
+void startStop(bool, int, void*) {
+   player.setActive(!player.isActive());
+}
+
+void setup() {
+  Serial.begin(115200);
+  AudioToolsLogger.begin(Serial, AudioToolsLogLevel::Info);
+
+  // setup output
+  auto cfg = kit.defaultConfig(TX_MODE);
+  // sd_active is setting up SPI with the right SD pins by calling 
+  // SPI.begin(PIN_AUDIO_KIT_SD_CARD_CLK, PIN_AUDIO_KIT_SD_CARD_MISO, PIN_AUDIO_KIT_SD_CARD_MOSI, PIN_AUDIO_KIT_SD_CARD_CS);
+  cfg.sd_active = true;
+  kit.begin(cfg);
+
+  // setup additional buttons 
+  kit.addDefaultActions();
+  kit.addAction(kit.getKey(1), startStop);
+  kit.addAction(kit.getKey(4), next);
+  kit.addAction(kit.getKey(3), previous);
+
+
+  // setup player
+  player.setVolume(0.7);
+  player.begin();
+
+  // select file with setPath() or setIndex()
+  //player.setPath("/ZZ Top/Unknown Album/Lowrider.mp3");
+  //player.setIndex(1); // 2nd file
+
+}
+
 void loop() {
-    copier.copy();
+  player.copy();
+  kit.processActions();
+
 }
