5f81f4d40142d73787430e2d0cc3e59729fc2d24
CaffeinatedOpe <caffeinatedope@proton.me>
2026-04-04 16:57:19 -0500

    the alarm alarms

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@
 
 #include "htmlData.h"
 
+#include <WiFi.h>
+#include <AsyncTCP.h>					 //https://github.com/me-no-dev/AsyncTCP using the latest dev version from @me-no-dev
+#include <ESPAsyncWebServer.h> //https://github.com/me-no-dev/ESPAsyncWebServer using the latest dev version from @me-no-dev
+#include <DNSServer.h>
+
 using namespace std;
 
 // don't ask
@@ -18,35 +23,10 @@ Display display = Display();
 
 typedef enum
 {
-	BUTTON_PRESSED,
-	BUTTON_NOT_PRESSED,
-	TIME_UPDATE,
-	IS_TIME,
-	IS_NOT_TIME
+	SOUNDING_ALARM,
+	IDLE
 } Status;
-
-#include <WiFi.h>
-#include <AsyncTCP.h>					 //https://github.com/me-no-dev/AsyncTCP using the latest dev version from @me-no-dev
-#include <ESPAsyncWebServer.h> //https://github.com/me-no-dev/ESPAsyncWebServer using the latest dev version from @me-no-dev
-#include <DNSServer.h>
-
-int receivedSeconds;
-int receivedMinutes;
-int receivedHours;
-
-typedef enum
-{
-	READY,
-	UPDATETIME,
-	UPDATEVOLUME,
-	UPDATECOLOR,
-	UPDATEDOTCOLOR
-} WIFISTATE;
-
-int receivedScreenColor[3];
-int receivedScreenDotColor[3];
-
-WIFISTATE wifiState = READY;
+Status alarmStatus = IDLE;
 
 class CaptiveRequestHandler : public AsyncWebHandler
 {
@@ -78,13 +58,7 @@ public:
 				inputSeconds = request->getParam("seconds")->value();
 				inputMinutes = request->getParam("minutes")->value();
 				inputHours = request->getParam("hours")->value();
-				wifiState = UPDATETIME;
-				Serial.println(inputSeconds.toInt());
-				Serial.println(inputMinutes.toInt());
-				Serial.println(inputHours.toInt());
 				manualTimeSetup(inputHours.toInt(), inputMinutes.toInt(), inputSeconds.toInt());
-				Serial.println(getMinutes());
-				Serial.println(getHours());
 			}
 			request->send(200, "text/plain", "OK");
 		}
@@ -163,15 +137,8 @@ void wifiSetup()
 	server.begin();
 }
 
-void updateWifi()
-{
-	if (wifiState == UPDATETIME)
-	{
-		manualTimeSetup(receivedHours, receivedMinutes, receivedSeconds);
-		wifiState = READY;
-	}
-}
-
+// put things in here that only need to run every loop *sometimes*
+// remove when not needed, add when needed. keeps loops optimized.
 vector<void (*)()> loopFunctions = {};
 
 void processButtons()
@@ -192,6 +159,7 @@ void processButtons()
 			break;
 		case Buttons::RIGHT_PRESS:
 			Serial.println("right press");
+			alarmStatus = IDLE;
 			toggleAudioState();
 			if (audioStatus == SILENT)
 			{ // this will be post-state swap
@@ -217,11 +185,13 @@ void processButtons()
 
 unsigned long displayTimeMillis; // used to lower the frequency of screen updates
 int oldminutes;
-void updateTime()
+void updateTimeDisplay()
 {
 	unsigned long currentmillis = millis();
-	if (currentmillis - displayTimeMillis >= 1000) // updates every second, can be easily adjusted.
-	{ 
+	int diff = currentmillis - displayTimeMillis;
+	if (abs(diff) >= 1000) // updates every second, can be easily adjusted.
+	// abs is used for the case where the millis reading hits a max and loops over, which will happen when the device is left on
+	{
 		if (oldminutes != getMinutes())
 		{
 			display.writeTime(getMinutes(), getHours());
@@ -231,43 +201,91 @@ void updateTime()
 	}
 }
 
-// put things in here that only need to run every loop *sometimes*
+struct Time
+{
+	int minutes;
+	int hours;
+	Time(int h, int m) {hours = h; minutes = m;}
+};
+
+vector<Time> alarms = {};
+
+// this alarm will NEVER TRIGGER with these values
+// cause, y'know, time
+Time nextAlarm(25, 61);
+
+int currentAlarmIndex = -1;
+
+// relies on the list of alarms being sorted, which should be done on boot (if alarms are saved) and each time an alarm is added
+// TODO: sorting algo is currently not implemented, needs implemented
+void getNextAlarm()
+{
+	currentAlarmIndex++;
+	if (currentAlarmIndex >= alarms.size())
+	{
+		currentAlarmIndex = 0;
+	}
+	nextAlarm = alarms[currentAlarmIndex];
+}
+
+void getCurrentAlarmIndex() { //this correctly sets the next alarm, not just the index
+	Time currentTime = Time(getHours(), getMinutes());
+	for (int i = 0; i < alarms.size(); i++) {
+		if (alarms[i].hours > currentTime.hours){
+			currentAlarmIndex = i;
+			nextAlarm = alarms[i];
+			break;
+		}
+	}
+}
+
+unsigned long alarmTimeMillis; // used to lower the frequency of screen updates
+void alarmLoop()
+{
+	unsigned long currentmillis = millis();
+	int diff = currentmillis - alarmTimeMillis;
+	if (abs(diff) >= 1000) // updates every second, can be easily adjusted.
+	// abs is used for the case where the millis reading hits a max and loops over, which will happen when the device is left on
+	{
+		if (nextAlarm.hours == getHours())
+		{
+			if (nextAlarm.minutes == getMinutes())
+			{
+				Serial.println("Sounding Alarm");
+				startAudio();
+				loopFunctions.push_back(playAudioLoop);
+				alarmStatus = SOUNDING_ALARM;
+				getNextAlarm();
+			}
+		}
+		alarmTimeMillis = currentmillis;
+	}
+}
 
 void setup()
 {
 	Serial.begin(115200);
 	audioSetup();
 	wifiSetup();
-	//  autoTimeSetup();
 	manualTimeSetup(7, 30, 0);
 
-	// ledsetup();
 	buttons.init();
 	display.init();
 	loopFunctions.push_back(processButtons);
-	loopFunctions.push_back(updateTime);
-	// writeString("testing");
+	loopFunctions.push_back(updateTimeDisplay);
+	loopFunctions.push_back(alarmLoop);
+
+	alarms.push_back(Time(7,31));
+	alarms.push_back(Time(7,35));
+	alarms.push_back(Time(8,0));
+	getCurrentAlarmIndex(); //this correctly sets the next alarm
 }
 
 void loop()
 {
-	// manualTimeLoop();
 	for (int i = 0; i < loopFunctions.size(); i++)
 	{
 		loopFunctions[i]();
 	}
 	dnsServer.processNextRequest();
-	/*switch(getManualAlarmTimeCompare("07:30:30")){
-		 case IS_TIME:
-				Serial.println("Time is Time");
-				switch(buttonL.getState())
-					 case BUTTON_PRESSED:
-							Serial.println("Button is Pressed");
-							break;
-					 case BUTTON_NOT_PRESSED:
-							Serial.println("Button is not Pressed");
-							break;
-
-				break;
-	}*/
 }
